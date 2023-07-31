70c6e1363f881f1c313925ca9acf0e02d22544ea
mbuimbogo <mbuimbogo99@gmail.com>
2023-07-31 16:06:09 +0300

    Add routing to render different screens/pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import LoginPage from './container/Login/LoginPage';
+import HomePage from './container/Home/HomePage';
+import Dashboard from './container/Home/Dashboard/Dashboard';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<LoginPage />}> </Route>
+        <Route path='/home' element={<HomePage />}> </Route>
+        <Route path='/dashboard' element={<Dashboard />}> </Route>
+      </Routes>
     </div>
   );
 }
