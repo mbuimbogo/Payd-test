1622ae7a85a4aaf3aa290493600b0eaff00b93d5
mbuimbogo <mbuimbogo99@gmail.com>
2023-07-31 16:05:38 +0300

    Initialize react TS template

---
+++ src/utils/index.ts
@@ -0,0 +1,141 @@
+// import mpesa from '';
+// import buyGoods from '/public/campaigns/buy_goods.svg';
+// import userConf from '/campaigns/user-configurations.svg';
+// import depoIcon from '/campaigns/deposit_creation.svg';
+export const getReleases = [
+  {
+    id: 1,
+    title: 'Deposit Creation',
+    name: 'Mustapha Karim',
+    src: '/mpesa_xpress.svg',
+    number: '0712345678',
+    ammount: '- 200000',
+    date: '2017-06-15'
+  },
+    {
+      id: 2,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 3,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 4,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 5,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 6,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 7,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 8,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 9,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 10,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 11,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 12,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 13,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 14,
+      title: 'Deposit Creation',
+      name: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+    {
+      id: 15,
+      title: 'Deposit Creation',
+      namee: 'Mustapha Karim',
+      src: '/mpesa_xpress.svg',
+      number: '0712345678',
+      ammount: '- 200000',
+      date: '2017-06-15'
+    },
+  ];
